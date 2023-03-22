9bd1c42d6678e6b0cfb540aa7cda6f3d6b37f530
Nikhil Dabhade <nikhil.k@accolite.com>
2023-03-22 14:45:07 +0530

    SpringBoot-Maven setup

---
+++ src/main/java/com/ecommerce/Qkart/QkartApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.Qkart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QkartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QkartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/Qkart/QkartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.Qkart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QkartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
