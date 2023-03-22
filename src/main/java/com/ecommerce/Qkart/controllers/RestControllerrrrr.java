3668bad3af2af34c672e2164df6a69e74c9bedb0
Nikhil Dabhade <nikhilkd1999@gmail.com>
2023-03-22 21:57:29 +0530

    Update

--- src/main/java/com/ecommerce/Qkart/controllers/RestControllerrrrr.java
+++ src/main/java/com/ecommerce/Qkart/controllers/RestControllerrrrr.java
@@ -1,12 +1,18 @@
 package com.ecommerce.Qkart.controllers;
 
 
+import com.ecommerce.Qkart.services.IAddressService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
-@org.springframework.web.bind.annotation.RestController
-public class RestController {
+@RestController
+public class RestControllerrrrr {
+
+    @Autowired
+    private IAddressService addressService;
 
     @GetMapping("/{name}")
     public ResponseEntity<String> getHomePage(@PathVariable String name) {
