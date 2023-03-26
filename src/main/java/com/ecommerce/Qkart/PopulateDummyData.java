9dea9b03c14a97c3fff108c4f5817a8482813607
Nikhil Dabhade <nikhilkd1999@gmail.com>
2023-03-26 16:54:24 +0530

    Update

---
+++ src/main/java/com/ecommerce/Qkart/PopulateDummyData.java
@@ -0,0 +1,95 @@
+package com.ecommerce.Qkart;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.ecommerce.Qkart.entities.Address;
+import com.ecommerce.Qkart.entities.Product;
+import com.ecommerce.Qkart.entities.ProductCategory;
+import com.ecommerce.Qkart.entities.User;
+import com.ecommerce.Qkart.repositories.IAddressRepository;
+import com.ecommerce.Qkart.repositories.IProductRepository;
+import com.ecommerce.Qkart.repositories.IUserRepository;
+
+@Component
+public class PopulateDummyData implements CommandLineRunner {
+
+    @Autowired
+    private IAddressRepository addressRepository;
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    @Autowired
+    private IProductRepository productRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        productRepository.deleteAll();
+        addressRepository.deleteAll();
+        userRepository.deleteAll();
+        initData();
+    }
+
+    private void initData() {
+        Address address1 = Address.builder()
+                .area("Used to be in my heart")
+                .house_no("69")
+                .build();
+
+        Address address2 = Address.builder()
+                .area("Park Avenue Society")
+                .city("Pune")
+                .pincode("444001")
+                .house_no("69")
+                .build();
+
+        addressRepository.saveAll(Arrays.asList(address1, address2));
+
+        User user = User.builder()
+                .name("Avni")
+                .email("avnipats@gmail.com")
+                .password("Nikhil")
+                .phone("69696969")
+                .address(Arrays.asList(address1, address2))
+                .build();
+        userRepository.save(user);
+
+        Product product1 = Product.builder()
+                .name("Samsung Galaxy S21")
+                .description("Flagship Android smartphone")
+                .category(ProductCategory.ELECTRONICS)
+                .price(999.0)
+                .quantityInStock(20)
+                .productImageUrl(
+                        "https://m.media-amazon.com/images/I/71qZERyxy6L._SX679_.jpg")
+                .build();
+
+        Product product2 = Product.builder()
+                .name("Apple MacBook Pro")
+                .description("High-performance laptop for professionals")
+                .category(ProductCategory.ELECTRONICS)
+                .price(2499.0)
+                .quantityInStock(5)
+                .productImageUrl(
+                        "https://m.media-amazon.com/images/I/61LNGJEMh0L._SX679_.jpg")
+                .build();
+
+        Product product3 = Product.builder()
+                .name("Nike Air Zoom Pegasus 38")
+                .description("Running shoes for men")
+                .category(ProductCategory.SPORTS)
+                .price(129.0)
+                .quantityInStock(30)
+                .productImageUrl(
+                        "https://m.media-amazon.com/images/I/51jTWpJXc7S._AC_UL480_FMwebp_QL65_.jpg")
+                .build();
+
+        productRepository.saveAll(List.of(product1, product2, product3));
+
+    }
+}
--- src/main/java/com/ecommerce/Qkart/controllers/RestControllerrrrr.java
+++ src/main/java/com/ecommerce/Qkart/controllers/RestControllerrrrr.java
@@ -1,71 +1,16 @@
 package com.ecommerce.Qkart.controllers;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ecommerce.Qkart.entities.Address;
-import com.ecommerce.Qkart.entities.Product;
-import com.ecommerce.Qkart.entities.ProductCategory;
-import com.ecommerce.Qkart.entities.User;
-import com.ecommerce.Qkart.repositories.ICartRepository;
-import com.ecommerce.Qkart.repositories.IOrderRepository;
-import com.ecommerce.Qkart.repositories.IProductRepository;
-import com.ecommerce.Qkart.repositories.IProductReviewRepository;
-import com.ecommerce.Qkart.repositories.IUserRepository;
-import com.ecommerce.Qkart.services.IAddressService;
 
 @RestController
 public class RestControllerrrrr {
 
-        @Autowired
-        IAddressService addressService;
-
-        @Autowired
-        IUserRepository userRepository;
-
-        @Autowired
-        ICartRepository cartRepository;
-
-        @Autowired
-        IOrderRepository orderRepository;
-
-        @Autowired
-        IProductRepository productRepository;
-
-        @Autowired
-        IProductReviewRepository reviewRepository;
-
         @GetMapping("/{name}")
         public ResponseEntity<String> getHomePage(@PathVariable String name) {
-
-                Address address1 = Address.builder().area("Ranipise Nagr").city("Akola")
-                                .pincode("444001").house_no("6969").build();
-
-                Address address2 = Address.builder().area("Partur Nagr").city("Partuer")
-                                .pincode("444001").house_no("6969").build();
-
-                User user = User.builder().name("Avni").email("avnipats@gmail.com")
-                                .address(List.of(address1, address2)).password("pass69").phone("69696969")
-                                .orders(null).cart(null)
-                                .build();
-
-                Product product = Product.builder().category(ProductCategory.ELECTRONICS).description("New Mobile")
-                                .name("I-Phone 14").price(150000.0).quantityInStock(10)
-                                .productImageUrl(
-                                                "https://images.unsplash.com/photo-1677563277026-17a254ea02f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1964&q=80")
-                                .build();
-
-                productRepository.save(product);
-
-                addressService.save(address1);
-                addressService.save(address2);
-
-                userRepository.save(user);
 
                 return ResponseEntity.ok("Hi " + name);
         }
